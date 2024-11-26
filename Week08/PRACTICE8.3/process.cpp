216070de6bc9236ab88aab482b8049edbdca8653
vuhieu0808 <minhhieuvutran046@gmail.com>
2024-11-26 19:50:08 +0700

    last commit

---
+++ Week08/PRACTICE8.3/process.cpp
@@ -0,0 +1,20 @@
+#include "process.h"
+
+char *solve(void) {
+    char *res = NULL;
+    int sz = 0, len = 1;
+    char c, prev = 'z';
+    res = (char *) calloc(1, sizeof(char));
+    while (true) {
+        c = getchar();
+        if (c == '\n' && prev == '.') break;
+        if (sz + 1 >= len) {
+            len <<= 1;
+            res = (char *) realloc(res, len * sizeof(char));
+        }
+        res[sz++] = c;
+        prev = c;
+    }
+    res[sz] = '\0';
+    return res;
+}
